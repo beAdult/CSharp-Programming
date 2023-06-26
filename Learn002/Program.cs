7f1d65761b3da25841f84a620797a5d30c792f58
Nguyễn Quốc Đạt <132989337+changchang2610@users.noreply.github.com>
2023-06-26 22:00:17 +0700

    Add files via upload

---
+++ Learn002/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+// public thành viên có thể truy cập được bởi code bât kỳ đâu, ngoài đối tượng, không có hạn chế truy cập nào.
+// private phương thức, thuộc tính, trường khai báo với private chỉ có thể truy cập, gọi bởi các dòng code cùng lớp.
+// protected phương thức, thuộc tính, trường chỉ có thể truy cập, gọi bởi các dòng code cùng lớp hoặc các lớp kế thừa nó.
+// internal truy cập được bởi code ở cùng assembly (file).
+// protected internal truy cập được từ code assembly, hoặc lớp kế thừa nó ở assembly khác.
+// private protected truy cập được code khi cùng assembly trong cùng lớp, hoặc các lớp kế thừa nó.
+// override methods
+// IsDispose and key word Using
+namespace Learn002
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student std = new Student("B20DCPT053", "Nguyen Quoc Dat", 22);
+
+            Console.WriteLine(std.ToString());
+        }
+    }
+}
---
+++ Learn002/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn002
+{
+    internal class Student
+    {
+        private string id, name;
+        private int age;
+
+
+        public Student()
+        {
+            this.id = this.name = "";
+            this.age = 0;
+        }
+
+        public Student(string id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        ~Student()
+        {
+            Console.WriteLine("Destroyed " + this.name);
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public String Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+
+        public override string ToString()
+        {
+            return "Id: " + id + " " + "Name is: " + name + " " + "Age is: " + age;
+        }
+    }
+}
