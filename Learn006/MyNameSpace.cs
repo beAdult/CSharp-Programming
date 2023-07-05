7c03719747d0e9e48efe6f7797ad14a6c2b19379
Nguyễn Quốc Đạt <134251073+nqdat02@users.noreply.github.com>
2023-07-05 09:56:12 +0700

    Namespace & Inherit in Class

---
+++ Learn006/MyNameSpace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNameSpace
+{
+    internal class Hello
+    {
+        public static void SayHello(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    namespace child
+    {
+        internal class Hi
+        {
+            public static void SayHi(string message)
+            {   
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
