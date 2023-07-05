2f943776b09509b6d0ea04b0b2dbeb08feb067da
Cheng Cheng <132989337+changchang2610@users.noreply.github.com>
2023-07-05 10:52:18 +0700

    Class Library

---
+++ Learn018/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Learn018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String html = "Ví dụ sử dụng HtmlHelper".HtmlTag("div", "text-danger");
+            Console.WriteLine(html);
+
+            "abc".HtmlTag("p");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
